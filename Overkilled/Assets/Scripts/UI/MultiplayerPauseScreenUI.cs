698f5f83f1849525796abf38513f88000f11dab3
Fillip Ngo Trung <thanhngotrung1345@gmail.com>
2024-10-24 18:43:09 +0100

    Change pausing behaviour
    Only the host may pause the game now. Clients may open pause menu to leave but this won't pause the game

--- Overkilled/Assets/Scripts/UI/MultiplayerPauseScreenUI.cs
+++ Overkilled/Assets/Scripts/UI/MultiplayerPauseScreenUI.cs
@@ -1,14 +1,18 @@
 using SurvivalGame;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class MultiplayerPauseScreenUI : MonoBehaviour
 {
     void Start()
     {
-        GameManager.Instance.OnMultiplayerGamePause += Show;
-        GameManager.Instance.OnMultiplayerGameUnpause += Hide;
+        if (!NetworkManager.Singleton.IsServer)
+        {
+            GameManager.Instance.OnMultiplayerGamePause += Show;
+            GameManager.Instance.OnMultiplayerGameUnpause += Hide;
+        }
 
         Hide();
     }
